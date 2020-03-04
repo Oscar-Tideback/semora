4af3913e13fd13f9ade490412bd83d0be71730d0
void00 <tideback@gmail.com>
2020-03-04 13:49:38 +0100

    Agents update

---
+++ www/js/AgentsPage.js
@@ -0,0 +1,23 @@
+class AgentsPage extends Base {
+
+  render() {
+    return /*html*/`
+      <div class="row" route="/real-estate-agents" page-title="Dhyr & Rumson - Våra mäklare">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm">
+              One of three columns
+            </div>
+            <div class="col-sm">
+              One of three columns
+            </div>
+            <div class="col-sm">
+              One of three columns
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+}
