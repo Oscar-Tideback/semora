13c3b9962be0efbd62f955958d7ab2c68a9fea61
void00 <tideback@gmail.com>
2020-03-04 18:18:40 +0100

    select from agents

--- www/js/AgentsPage.js
+++ www/js/AgentsPage.js
@@ -1,18 +1,26 @@
 class AgentsPage extends Base {
 
+  async mount() {
+    this.foundAgents = [];
+    await sql(/*sql*/`USE DhyrRumson.db`);
+    this.foundAgents = await sql(/*sql*/`
+    SELECT firsteName, lastName, email FROM user, userTyp WHERE user.id = userTyp.userId AND isAgent = "true"
+    `);
+  }
+
   render() {
     return /*html*/`
       <div class="row" route="/real-estate-agents" page-title="Dhyr & Rumson - Våra mäklare">
         <div class="container">
           <div class="row">
             <div class="col-sm">
-              One of three columns
+              ${this.foundAgents[0].firsteName}
             </div>
             <div class="col-sm">
-              One of three columns
+              ${this.foundAgents[0].lastName}
             </div>
             <div class="col-sm">
-              One of three columns
+            ${this.foundAgents[0].email}
             </div>
           </div>
         </div>
