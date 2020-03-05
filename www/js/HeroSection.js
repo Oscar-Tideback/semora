e9632e7b9d9794395b98a23cfebae26c7e00bc4b
RikardLof <rikardlof@gmail.com>
2020-03-05 19:44:56 +0100

    Ändring av layout sökfält och hero startsida

--- www/js/HeroSection.js
+++ www/js/HeroSection.js
@@ -3,9 +3,7 @@ class HeroSection extends Base {
   render() {
     return /*html*/`
       <div class="hero">
-        <div class="row">
           <h1>HERO</h1>
-        </div>
       </div>
     `;
   }
