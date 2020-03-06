1b7b4096e7789aacbf286fa8436a6fcc2abd5940
void00 <tideback@gmail.com>
2020-03-06 02:20:29 +0100

    .

--- www/js/AgentsPage.js
+++ www/js/AgentsPage.js
@@ -13,14 +13,22 @@ class AgentsPage extends Base {
       <div class="row" route="/real-estate-agents" page-title="Dhyr & Rumson - Våra mäklare">
         <div class="container">
           <div class="row">           
-            <!--<img src="images/${this.foundAgents[3].imageUrl}" alt="Agent face">-->   
-              <div class="col-sm-9">
-               ${this.foundAgents.map(user => /*html*/`<div class="col-sm-3"><img src="images/${user.imageUrl}" alt="Agent face"></div>` + '' + /*html*/`<div class="col-sm-6">${user.lastName}</div>`)}             
+            <!--<img src="images/${this.foundAgents[3].imageUrl}" alt="Agent face">     ` + '' + /*html*/`     -->   
+              <div class="col-sm-9">Våra mäklare.
+               ${this.foundAgents.map(user => /*html*/`
+                <div class="col-3"><img src="images/${user.imageUrl}"  class="img-fluid img-thumbnail" alt="Agent face">
+                <div class="col-6">${user.lastName}son?
+                </div>
+                </div>
+              `)}             
             </div>
           </div>
         </div>
       </div>
     `;
+  }
+  doSomeThing() { 
+    
   }
 
 }
