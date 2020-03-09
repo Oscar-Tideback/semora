e6fd880124621caa8f76359f2daf511f1369f6b2
mdaoud <daoud_mohamad@yahoo.co.uk>
2020-03-09 20:36:50 +0100

    add a new search form...

--- www/js/BuyerPage.js
+++ www/js/BuyerPage.js
@@ -26,7 +26,7 @@ class BuyerPage extends Base {
     if (['ArrowUp', 'ArrowDown'].includes(e.key)) { return; }
 
     if (e.key === 'Enter' && this.selected >= 0) {
-      this.chosen = this.foundKeywords[this.selected].regionName;
+      this.chosen = this.foundKeywords[this.selected];
       this.foundKeywords = [];
       this.selected = -1;
       this.render();
@@ -46,36 +46,21 @@ class BuyerPage extends Base {
         <div class="row m-0" route="/buy-property" page-title="Köpa bostad">
         <div class="col-12">
         <h1>Köpa Bostad </h1>
-        <p>Sökfilter</p>
-        <h2>Resultatlista</h2>
-        
-        <div class="search-in-hero-holder pr-4">
-        <form class="navbar-form search-in-hero" action="/buy-property" id="navBarSearch">
-          <div class="input-group">
-            <input type="text" class="form-control" placeholder="Snabbsök bostad här..." keyup="searchKeyword" keydown="selectWithUpDownArrows" autocomplete="off" autocorrect="off">
+        <div class="active-pink-3 active-pink-4 mb-4">
+          <input class="form-control" type="text" placeholder="Snabbsök bostad här..." aria-label="Search" keyup="searchKeyword" keydown="selectWithUpDownArrows" autocomplete="off" autocorrect="off">
             ${this.foundKeywords.length < 1 ? '' : /*html*/`
-              <div class="dropdown-menu show position-absolute">
-                  ${this.foundKeywords.map((keywords, index) => /*html*/`
-                  
-                    <div class="col-4"><p>${foundKeywords.regionName}` + ' ' + `${foundKeywords.price}</p><p>${foundKeywords.tenure}</p><p>${foundKeywords.area}</p></div>)} </div>               
+            ${this.foundKeywords.map((keywords, index) => /*html*/`
+            <div class="col-4"><p>${foundKeywords.rooms}` + ' ' + `${foundKeywords.price}</p><p>${foundKeywords.tenure}</p><p>${foundKeywords.area}</p></div>)} </div>               
                                   
-                  `)
-        }
-              </div>
-  `}
-            <div class="input-group-btn">
-              <button class="btn btn-default pull-right" type="submit">
-                <i class="icofont-search icofont-lg navbar-search-icon"></i>
-              </button>
-            </div>
-          </div>
-        </form>
-        
-      </div>  
-      
-      </div >
-      </div >
+                  `)}
+  }
 
+  `}
+        </div>
+        <p>Sökfilter</p>
+        <h2>Resultatlista</h2> 
+      </div >
+      </div >
       `
   }
 
