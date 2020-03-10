bbf29580122d8fc4cd80ab1193cf7461d93832c5
void00 <tideback@gmail.com>
2020-03-10 20:42:53 +0100

    code cleaning agentspage

--- www/js/AgentsPage.js
+++ www/js/AgentsPage.js
@@ -19,30 +19,35 @@ class AgentsPage extends Base {
     return /*html*/`
       <div class="row m-0" route="/real-estate-agents" page-title="Dhyr & Rumson - Våra mäklare">  
         <div class="container my-4"> 
-          <div class="row">
-          
+          <div class="row">     
             <div class="col-12">    
               <div class="row">
                 <div class="col-8"><h5>Lär känna våra våra mäklare.</h5>
-                <p>Kunskap och erfarenhet är tillgångar i alla yrken.</p>
-                <p>Till Dhyr & Rumson har vi därför handplockat endast dom som heter son i efternamn och de skickligaste och mest erfarna mäklarna i Stockholm.
-                Vi har gjort det av en enda anledning – så att rätt person kan företräda dig i din kanske största affär.</p>
+                  <p>Kunskap och erfarenhet är tillgångar i alla yrken.</p>
+                  <p>Till Dhyr & Rumson har vi därför handplockat endast dom som heter son i efternamn och de skickligaste och mest erfarna mäklarna i Stockholm.
+                  Vi har gjort det av en enda anledning – så att rätt person kan företräda dig i din kanske största affär.</p>
                 </div>
               </div>
               <div class="row">
                 ${this.foundAgents.map(user => /*html*/`
-                <div class="col-2"><a href="/real-estate-agent" click="refreshBroker" targetbrokerid="${user.id}"><img src="images/${user.imageUrl}"  targetbrokerid="${user.id}"  class="img-fluid img-thumbnail" alt="Agent face"></a></div>
-                <div class="col-4"><p  targetbrokerid="${user.id}">${user.firstName}` + ' ' + `${user.lastName}</p></a><p>${user.email}</p><p>${user.phone}</p></div>`)}</div>              
-                </div>
-
-                
-
+                  <div class="col-2">
+                    <a href="/real-estate-agent" click="refreshBroker" targetbrokerid="${user.id}">
+                    <img src="images/${user.imageUrl}"  targetbrokerid="${user.id}"  class="img-fluid img-thumbnail" alt="Agent face"></a>
+                  </div>
+                  <div class="col-4">
+                    <a href="/real-estate-agent" click="refreshBroker" targetbrokerid="${user.id}">
+                    ${user.firstName}` + ' ' + /*html*/`
+                    ${user.lastName}</a><p>                  
+                    ${user.email}<br>
+                    ${user.phone}</p>
+                  </div >
+                `)}
+                </div>              
               </div>
-              
-            </div>
+            </div>  
           </div>
         </div>
-      </div>
+      </div>   
     `;
   }
 }
