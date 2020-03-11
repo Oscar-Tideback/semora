354f2a1d4cb4189ba8ec5d93c35cdd6cd47e805a
A.falle <fallleee123@gmail.com>
2020-03-11 17:06:13 +0100

    Ändring av col storlek

--- www/js/ObjectsPage.js
+++ www/js/ObjectsPage.js
@@ -24,18 +24,16 @@ class ObjectsPage extends Base {
     render() {
         return /*html*/`
             <div class= "row m-0" route="/real-estate-info" page-title="Bostad info">
-                <div class= "container">
-                    <div class= "row">
+                <div class= "container my-3">
+                    <div class= "row p-5">
                         <div class="col-sm-9"> Objekt Information.
                         ${this.foundObjects.map(realEstateInfo => /*html*/`
-                            <div class="col-3"><img src="images/${realEstateInfo.imgUrl}.jpg"  class="img-fluid img-thumbnail" alt="bostad picture">
-                            <div class="col-3">${realEstateInfo.area}</div>
-                            <div class="col-3">${realEstateInfo.rooms}</div>
-                            <div class="col-3">${realEstateInfo.buildYear}</div>
-                            <div class="col-3">${realEstateInfo.maintenanceCost}</div>
-                            <div class="col-3">${realEstateInfo.tenure}</div>
-                            <div class="col-3">${realEstateInfo.price}</div>
-                        
+                            <div class="col-2"><img src="images/${realEstateInfo.imgUrl}.jpg"  class="img-fluid img-thumbnail" alt="bostad picture">
+                            <div class="col-2">${realEstateInfo.area}</div>
+                            <div class="col-2">${realEstateInfo.rooms}</div>
+                            <div class="col-2">${realEstateInfo.buildYear}</div>
+                            <div class="col-2">${realEstateInfo.maintenanceCost}</div>
+                            <div class="col-2">${realEstateInfo.tenure}</div>                        
                             <!--<div class="col-1">${realEstateInfo.description}</div>-->
                         `)}
                         </div>
