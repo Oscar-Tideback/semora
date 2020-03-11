71888acaa0b4384dc9e5947ae564dca616892d6d
A.falle <fallleee123@gmail.com>
2020-03-11 19:21:21 +0100

    Objektsida

--- www/js/ObjectsPage.js
+++ www/js/ObjectsPage.js
@@ -26,8 +26,13 @@ class ObjectsPage extends Base {
             <div class= "row m-0" route="/real-estate-info" page-title="Bostad info">
                 <div class= "container my-3">
                     <div class= "row p-5">
-                        <div class="col-sm-9"> Objekt Information.
+                    <h1> Objekt Information </h1>
+                        <div class="col-sm-9">
                         ${this.foundObjects.map(realEstateInfo => /*html*/`
+                        <div class="col d-flex justify-content-left">
+                        
+
+                        
                             <div class="col-2"><img src="images/${realEstateInfo.imgUrl}.jpg"  class="img-fluid img-thumbnail" alt="bostad picture">
                             <div class="col-2">${realEstateInfo.area}</div>
                             <div class="col-2">${realEstateInfo.rooms}</div>
@@ -40,6 +45,9 @@ class ObjectsPage extends Base {
                     </div>
                 </div>
             </div>
+        </div>
+        
+    
         `;
     }
 }
