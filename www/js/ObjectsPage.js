652129e06ba10441fa4fa49adf5c4f22932c713c
void00 <tideback@gmail.com>
2020-03-12 18:17:23 +0100

    CodeCleaner action

--- www/js/ObjectsPage.js
+++ www/js/ObjectsPage.js
@@ -1,9 +1,13 @@
 class ObjectsPage extends Base {
 
+    async mount() {
+        //this.targetBostadId = app.objectsPage.targetBostadId;
+        return this.makeSql();
+    }
+
     async makeSql() {
         //Hämta objekt från databasen 
-        this.foundObjects = await sql(/*sql*/`
-    
+        this.foundObjects = await sql(/*sql*/` 
     SELECT realEstateInfo.area, realEstateInfo.rooms, realEstateInfo.description,
     realEstateInfo.buildYear, realEstateInfo.maintenanceCost, 
     realEstateInfo.tenure, realEstateInfo.price,
@@ -12,71 +16,61 @@ class ObjectsPage extends Base {
     WHERE realEstateInfo.Id = ${app.objectsPage.targetBostadId}
     AND realEstateImages.realEstateInfoId = ${app.objectsPage.targetBostadId}
     AND realEstateImages.imgUrl LIKE '%img01%'
-
     `);
-        //Objekt information från databasen om respektive objekt.
-        //Html nedan för layout och design targetBostadId
-    }
-    async mount() {
-        //this.targetBostadId = app.objectsPage.targetBostadId;
-        return this.makeSql();
+        //Objekt information från databasen om respektive objekt.     
     }
 
+    //Html nedan för layout och design targetBostadId
     render() {
         return /*html*/`
             <div class= "row m-0" route="/real-estate-info" page-title="Bostad info">
                 <div class= "container my-3">
                     <div class= "row p-5">
-                    <h1> Objekt Information </h1>
-                        <div class="col-sm-12">
-                        ${this.foundObjects.map(realEstateInfo => /*html*/`
-                        <div class="col d-flex justify-content-center">
-                        <div class="card my-8">
-                        
-                            <img src="images/${realEstateInfo.imgUrl}" class="card-img-top" alt="..." realEstateId="${realEstateInfo.Id}">
-                      <div class="card-body">
-                        <div class="card-text">
-                            ${realEstateInfo.streetName} 
-                            ${realEstateInfo.streetNumber}
-                            ${realEstateInfo.floor}<br>
-                            ${realEstateInfo.areaName},<br>
-                            ${realEstateInfo.regionName}<br>
-                            ${realEstateInfo.rooms} rum,<br>
-                            ${realEstateInfo.area} m²<br>
-                            ${realEstateInfo.price} kr<br>
-                            ${realEstateInfo.area}<br>
-                            ${realEstateInfo.rooms}<br>
-                            ${realEstateInfo.buildYear}<br>
-                            ${realEstateInfo.maintenanceCost}<br>
-                            ${realEstateInfo.tenure}<br>
-                            ${realEstateInfo.price}    <br>              
-                            ${realEstateInfo.description}
+                        <h1> Objekt Information </h1>
+                            <div class="col-sm-12">
+                                ${this.foundObjects.map(realEstateInfo => /*html*/`
+                                <div class="col d-flex justify-content-center">
+                                    <div class="card my-8">
+                                        <img src="images/${realEstateInfo.imgUrl}" class="card-img-top" alt="${realEstateInfo.Id}" realEstateId="${realEstateInfo.Id}">
+                                        <div class="card-body">
+                                            <div class="card-text">
+                                                ${realEstateInfo.streetName} 
+                                                ${realEstateInfo.streetNumber}
+                                                ${realEstateInfo.floor}<br>
+                                                ${realEstateInfo.areaName},<br>
+                                                ${realEstateInfo.regionName}<br>
+                                                ${realEstateInfo.rooms} rum,<br>
+                                                ${realEstateInfo.area} m²<br>
+                                                ${realEstateInfo.price} kr<br>
+                                                ${realEstateInfo.area}<br>
+                                                ${realEstateInfo.rooms}<br>
+                                                ${realEstateInfo.buildYear}<br>
+                                                ${realEstateInfo.maintenanceCost}<br>
+                                                ${realEstateInfo.tenure}<br>
+                                                ${realEstateInfo.price}    <br>              
+                                                ${realEstateInfo.description}
+                                            </div>   
+                                            `)}
+                                            <img class="d-block w-100" src="images/${this.targetBostadId}/img02.jpg"><br>
+                                            <img class="d-block w-100" src="images/${this.targetBostadId}/img03.jpg"><br>
+                                            <img class="d-block w-100" src="images/${this.targetBostadId}/img04.jpg"><br>
+                                            <img class="d-block w-100" src="images/${this.targetBostadId}/img05.jpg"><br>
+                                            <img class="d-block w-100" src="images/${this.targetBostadId}/img06.jpg"><br>
+                                            <img class="d-block w-100" src="images/${this.targetBostadId}/img07.jpg"><br>
+                                            <img class="d-block w-100" src="images/${this.targetBostadId}/img08.jpg"><br>
+                                            <img class="d-block w-100" src="images/${this.targetBostadId}/img09.jpg"><br>
+                                            <img class="d-block w-100" src="images/${this.targetBostadId}/img10.jpg"><br>
+                                            <img class="d-block w-100" src="images/${this.targetBostadId}/img11.jpg"><br>
+                                            <img class="d-block w-100" src="images/${this.targetBostadId}/img12.jpg"><br>
+                                            <img class="d-block w-100" src="images/${this.targetBostadId}/img13.jpg"><br>
+                                            <img class="d-block w-100" src="images/${this.targetBostadId}/img14.jpg"><br>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            
-                            
-                            
-                        `)}
-                        <img class="d-block w-100" src="images/${this.targetBostadId}/img02.jpg"><br>
-                        <img class="d-block w-100" src="images/${this.targetBostadId}/img03.jpg"><br>
-                        <img class="d-block w-100" src="images/${this.targetBostadId}/img04.jpg"><br>
-                        <img class="d-block w-100" src="images/${this.targetBostadId}/img05.jpg"><br>
-                        <img class="d-block w-100" src="images/${this.targetBostadId}/img06.jpg"><br>
-                        <img class="d-block w-100" src="images/${this.targetBostadId}/img07.jpg"><br>
-                        <img class="d-block w-100" src="images/${this.targetBostadId}/img08.jpg"><br>
-                        <img class="d-block w-100" src="images/${this.targetBostadId}/img09.jpg"><br>
-                        <img class="d-block w-100" src="images/${this.targetBostadId}/img10.jpg"><br>
-                        <img class="d-block w-100" src="images/${this.targetBostadId}/img11.jpg"><br>
-                        <img class="d-block w-100" src="images/${this.targetBostadId}/img12.jpg"><br>
-                        <img class="d-block w-100" src="images/${this.targetBostadId}/img13.jpg"><br>
-                        <img class="d-block w-100" src="images/${this.targetBostadId}/img14.jpg"><br>
-
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-</div>
-</div>
-</div>
-
-        `;
+    `;
     }
 }
