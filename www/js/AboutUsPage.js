e4145b26398c644f5ff710d4415f612a11b6a197
void00 <tideback@gmail.com>
2020-03-13 10:30:12 +0100

    Added son in database

--- www/js/AboutUsPage.js
+++ www/js/AboutUsPage.js
@@ -5,7 +5,7 @@ class AboutUsPage extends Base {
       <div class="row m-0 p-4" route="/about-us" page-title="Om oss">
         <div class="col-12">
           <h1>Vilka är vi</h1>
-          <p>Lite text</p>
+          <p>Det är som det ofta är med företag av alla de slag. Vissa satsar på volym – andra på kvalitet. <br>På Dhyr & Rumson försöker vi inte bli störst utan i stället satsar vi på att vara bäst. Vi tror att det lönar sig i längden, för oss och för våra kunder.</p>
         </div>
       </div>
     `;
