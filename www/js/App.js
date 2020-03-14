b47a2ed7646c9962782af5ea711ead06da5362ae
RikardLof <rikardlof@gmail.com>
2020-03-14 22:35:51 +0100

    Ändring

--- www/js/App.js
+++ www/js/App.js
@@ -11,7 +11,7 @@ class App extends Base {
       { label: 'Våra mäklare', route: '/real-estate-agents' },
       { label: 'Bostad info', route: '/real-estate-info' },
       { label: 'Om oss', route: '/about-us' },
-      { label: 'Rikards testsida', route: '/testpage' }
+      { label: 'Rikards testsida', route: '/testpage' },
 
       { label: '', route: '/integrity' }// This is so wrong, fix it asap
 
