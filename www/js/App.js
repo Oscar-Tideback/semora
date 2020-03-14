7a515c58f2dfa2e09258fc1c6efbd8f369a380e2
RikardLof <rikardlof@gmail.com>
2020-03-14 22:25:54 +0100

    Ändring

--- www/js/App.js
+++ www/js/App.js
@@ -11,7 +11,7 @@ class App extends Base {
       { label: 'Våra mäklare', route: '/real-estate-agents' },
       { label: 'Bostad info', route: '/real-estate-info' },
       { label: 'Om oss', route: '/about-us' },
-      { label: 'En testsida', route: '/testpage' }
+      { label: 'Rikards testsida', route: '/testpage' }
 
 
     ];
