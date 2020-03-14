b3de40a621014318772ce2e79fdd6d3f092d8554
void00 <tideback@gmail.com>
2020-03-14 19:48:44 +0100

    Work in progress

--- www/js/AgentPage.js
+++ www/js/AgentPage.js
@@ -1,5 +1,22 @@
 class AgentPage extends Base {
 
+  async collectFormData(e) {
+    // Loop through the form and collect the input
+    let data = {};
+    for (let element of [...e.target.closest('form').elements]) {
+      if (!element.name) { continue; }
+      data[element.name] = element.value;
+    }
+    //send data sql;
+    e.preventDefault();
+    await sql(/*sql*/`
+      INSERT INTO potentialCustomer (name, email, phone, subject) 
+      VALUES($name, $email, $phone, $subject);
+    `, data);
+    this.sentForm = 'true';
+    this.render();
+  }
+
   async makeSql() {
     //De bubblar upp/fram?
     this.foundAgents = await sql(/*sql*/`
@@ -24,23 +41,49 @@ class AgentPage extends Base {
       <div class="row m-0 p-4" route="/real-estate-agent" page-title="Dhyr & Rumson - Våra mäklare">  
         <div class="container my-4"> 
           <div class="row">
-                <div class="col-12"><h5>Lär känna våra våra mäklare.</h5>
+              <div class="col-12"><h5>Lär känna våra våra mäklare.</h5>
                 <p>Kunskap och erfarenhet är tillgångar i alla yrken.</p>
-                
-                </div>
-              </div>
-              <div class="row">               
-                ${this.foundAgents.map(user => /*html*/`
-                <div class="col-2"><img src="images/${user.imageUrl}" style="max-width: 130px;" class="img-fluid img-thumbnail rounded float-left" alt="Agent face"></div>
-                <div class="col-6"><p>
-                ${user.firstName}` + ' ' + `${user.lastName}</p><p>
-                ${user.email}</p><p>
-                ${user.phone}</p>
-                ${user.region_names}</p>
-                </div>`)}</div>              
-              </div>
               </div>
             </div>
+                <div class="row">               
+                    ${this.foundAgents.map(user => /*html*/`
+                      <div class="col-3">
+                        <img src="images/${user.imageUrl}" style="max-width: 240px;" class="img-fluid img-thumbnail rounded float-left" alt="Agent face"></div>
+                      <div class="col-3"><p>
+                        ${user.firstName}` + ' ' + `${user.lastName}</p><p>
+                        ${user.email}</p><p>
+                        ${user.phone}</p>
+                        ${user.region_names}</p>
+                        </div>
+                    `)}
+                      <!--Form-->
+                      <form submit="collectFormData" class="col-6">
+                      <div class="md-form">
+                        <i class="fas fa-user prefix grey-text"></i>
+                        <label>Namn:</label>
+                        <input name="name" type="name" class="form-control">         
+                      </div>
+                      <div class="md-form">
+                        <i class="fas fa-envelope prefix grey-text"></i>
+                        <label>E-Mail:</label>
+                        <input name="email" type="email" class="form-control">
+                      </div>
+                      <div class="md-form">
+                        <i class="fas fa-envelope prefix grey-text"></i>
+                        <label>Telefon:</label>
+                        <input name="phone" type="phone" class="form-control">
+                      </div>
+                      <div class="md-form">
+                        <i class="fas fa-tag prefix grey-text"></i>
+                        <label>Ditt ärende:</label>
+                        <textarea name="subject" type="subject" class="form-control md-textarea" rows="3"></textarea>
+                      </div>
+                      <div class="text-center mt-4" >
+                        <button type="submit"  class="btn border btn-light-blue" value="Send">Skicka</button>
+                      </div>           
+                </div>
+            </div>
+          </div>
         </div>
       </div>
     `;
