2702401ec2005ae973de49660519ee5691e7b172
RikardLof <rikardlof@gmail.com>
2020-03-19 09:54:23 +0100

    Ändring

--- www/js/NavBarSearch.js
+++ www/js/NavBarSearch.js
@@ -54,7 +54,7 @@ class NavBarSearch extends Base {
           GROUP BY realEstateInfo.Id
           )
       GROUP BY regionName
-      ORDER BY totalHits DESC`
+      ORDER BY totalHits DESC, regionName ASC`
       , { text: '%' + e.target.value + '%' });
 
     this.currentKeyword = e.target.value;
@@ -93,7 +93,7 @@ class NavBarSearch extends Base {
             <input type="text" class="form-control nav-bar-search-input rounded" placeholder="Snabbsök bostad här..." keyup="searchKeyword" keydown="selectWithUpDownArrows" autocomplete="off" autocorrect="off">
             ${this.searchHits.length < 1 ? '' : /*html*/`
               <div class="dropdown-menu show position-absolute">
-                <div class="search-in-hero-item dropdown-item"><b>Antal 123 träffar på ${'"' + this.currentKeyword + '"'} per region</b></div>
+                <div class="search-in-hero-item dropdown-item"><b>Antal träffar på ${'"' + this.currentKeyword + '"'} per region</b></div>
                 ${this.searchHits.map((hits, index) => /*html*/`
                   <button click="clickKeyword" class="dropdown-item ${this.selected !== index ? '' : 'bg-primary text-light'}" type="button">
                     <div class="row">
