bc65e17c10d98f5071a4abdb9c87bf21732c2d26
void00 <tideback@gmail.com>
2020-03-26 15:01:02 +0100

    Design agent

--- www/js/AgentPage.js
+++ www/js/AgentPage.js
@@ -26,27 +26,31 @@ class AgentPage extends Base {
     //console.log(this);
     return /*html*/`
     <div class="row m-0 p-4" route="/real-estate-agent/${this.id}" page-title="Dhyr & Rumson - Våra mäklare">  
-    <div class="container my-4"> 
+    <div class="container"> 
       <div class="row">
-        <div class="col-12"><h5>Lär känna våra våra mäklare.</h5>
-          <p>Kunskap och erfarenhet är tillgångar i alla yrken.</p>
+        <div class="col-12">
+        <p class="name-bold broker-name">Lär känna, ${this.firstName} ${this.lastName}.</p>  
+        <p>${this.description}</p>
         </div>
       </div>
-        <div class="row">
+        <div class="row bg-warning ">
         
 
 
-          <div class="col-auto">
-              <img src="../images/${this.imageUrl}" class="img-thumbnail rounded float-left" alt="Agent face">
-          </div>
-            <div class="col-lg-3 col-md-12 col-sm-12">
-            <p class="name-bold broker-name">${this.firstName} ${this.lastName}</p>
+          <div class="col-lg-6 bg-info ">
+              <img src="../images/${this.imageUrl}" class="img-thumbnail rounded float-bottom" alt="Agent face">
               <p class="broker-name name-email-phone"><span class="name-bold">E-Mail:</span> ${this.email}</p>
               <p class="broker-name name-email-phone"><span class="name-bold">Tel:</span>  ${this.phone.toString().replace(/\B(?=(\d{3})+(\d{4})+(?!\d))/g, " ")}</p>
               <p class="broker-name name-region"><span class="name-bold">Region:</span> ${this.region_names}.</p>
-            </div>              
+          </div>
+            <div class="col-lg-6 col-md-12 col-sm-12">
+            
+              
+                         
             <!--Form-->
-            <form submit="collectFormData" class="col-lg-6 col-md-12 col-sm-12">
+            <div class="col-12 bg-danger ">
+            <form submit="collectFormData" class="col-lg-12 col-md-12 col-sm-12">
+            <div class="name-bold text-center">Kontakta mig.</div>
             <div class="md-form">
               <i class="fas fa-user prefix grey-text"></i>
               <label>Namn:</label>
@@ -68,7 +72,7 @@ class AgentPage extends Base {
               <textarea name="subject" type="subject" class="form-control md-textarea" rows="3"></textarea>
             </div>
             <div class="text-center mt-4" >
-              <button type="submit"  class="btn border btn-light-blue" value="Send">Skicka</button>
+              <button type="submit"  class="btn col-12 border btn-light-blue" value="Send">Skicka</button>
             </div>           
           </div>
         </div>    
@@ -81,7 +85,7 @@ class AgentPage extends Base {
                 </div>
                 <div class="modal-body">
                   <p>Ditt meddelande har skickats!<br>
-                  <p class="name-bold broker-name">${this.firstName} ${this.lastName} återkommer snarast till dig via telefon eller email.</p>
+                  <p><span class="name-bold broker-name">${this.firstName} ${this.lastName} </span>återkommer snarast till dig via telefon eller email.</p>
                 </div>
                 <div class="mt-4 modal-footer">
                   <a href="/" type="button" class="btn btn-primary float-left"  data-dismiss="modal">Till startsidan!</a>
