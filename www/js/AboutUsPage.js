9ed92cd1a18e4df3542485b27f7f89d3243e7a4f
void00 <tideback@gmail.com>
2020-03-27 12:33:11 +0100

    late graphic fix

--- www/js/AboutUsPage.js
+++ www/js/AboutUsPage.js
@@ -13,7 +13,7 @@ class AboutUsPage extends Base {
           </p>
           <h3>Vi ser fram emot att höra ifrån dig</h3>
           <p>Det är som det ofta är med företag av alla de slag. Vissa satsar på volym – andra på kvalitet. <br>
-          På BOSTHLM försöker vi inte bli störst utan i stället satsar vi på att vara bäst. 
+          På Dhyr & Rumson försöker vi inte bli störst utan i stället satsar vi på att vara bäst. 
           Vi tror att det lönar sig i längden, för oss och för våra kunder.</p>
           <h5><a href="/real-estate-agents">Kontakta någon av våra mäklare så får du veta mer</a></h5>
         </div>
